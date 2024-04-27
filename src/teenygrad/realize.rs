3889e05a86553340581b55aec4f290af4dd94b36
calexanderberg <carlalexanderberg@gmail.com>
2024-04-27 18:16:40 +0200

    Add: realize.rs, do not merge it is not even close to being done

---
+++ src/teenygrad/realize.rs
@@ -0,0 +1,9 @@
+pub fn run_scheduke(schedule: Vec<i32>, disable_logging: bool) -> RetType {
+    if disable_logging = False {
+        panic!("WORK ON ERROR MESSAGE");
+    }
+}
+
+pub fn create_schedule(outs: Vec<i32>, seen: Option<Vec<i32>>) -> Vec<i32> {
+    vec![]
+}
