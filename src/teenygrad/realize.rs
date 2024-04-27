6935c5a9edc3a9f3b69a1066ea6447db4beb130f
21st <60197499+21st-centuryman@users.noreply.github.com>
2024-04-27 19:02:35 +0200

    Fix: realize.rs line count

--- src/teenygrad/realize.rs
+++ src/teenygrad/realize.rs
@@ -1,7 +1,2 @@
-pub fn run_schedule(schedule: Vec<i32>) -> std::io::Result<()> {
-    return Ok();
-}
-
-pub fn create_schedule(outs: Vec<i32>) -> Vec<i32> {
-    return vec![];
-}
+pub fn run_schedule(schedule: Vec<i32>) -> std::io::Result<()> { return Ok(); }
+pub fn create_schedule(outs: Vec<i32>) -> Vec<i32> { return vec![];}
