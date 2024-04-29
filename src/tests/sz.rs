af50a363414702ee99804f0f926c8fcabc05a8bc
calexanderberg <carlalexanderberg@gmail.com>
2024-04-29 12:45:03 +0200

    Fix: errors in sz.rs, also much cleaner

--- src/tests/sz.rs
+++ src/tests/sz.rs
@@ -6,34 +6,37 @@ use walkdir::WalkDir;
 
 #[test]
 fn lines() -> std::io::Result<()> {
-    let token_whitelist = |token: &Token| {
-        matches!(
-            token,
-            Token::OP(_) | Token::Name(_) | Token::Number(_) | Token::String(_)
-        )
-    };
     let mut table: Vec<(String, i32, f32)> = vec![];
 
     for entry in WalkDir::new("src/teenygrad") {
         let filepath = entry.unwrap().path().to_string_lossy().to_string();
         if filepath.ends_with(".rs") {
-            let lines = BufReader::new(File::open(&filepath).unwrap())
+            let tokens = BufReader::new(File::open(&filepath).unwrap())
                 .lines()
                 .filter_map(|line| line.ok())
-                .filter(|line| !line.is_empty() && !line.starts_with("//"))
-                .collect::<Vec<_>>();
-            let _tokens = lines
-                .clone()
                 .into_iter()
                 .map(|item| tokenize(item.to_string()))
-                .filter_map(|token| token.ok())
-                .filter(|token| token.iter().any(&token_whitelist))
+                .map(|token_vec| {
+                    token_vec
+                        .unwrap()
+                        .into_iter()
+                        .filter(|token| {
+                            matches!(
+                                token,
+                                Token::Name(_) | Token::OP(_) | Token::Number(_) | Token::String(_) // Whitelisted tokens
+                            )
+                        })
+                        .collect::<Vec<_>>()
+                })
                 .collect::<Vec<_>>();
             table.push((
                 filepath.chars().skip(4).collect::<String>(),
-                lines.len() as i32,
-                0.0,
-                //(tokens.len() as f32) / (lines.len() as f32), // We will add this later
+                tokens.len() as i32,
+                (tokens
+                    .iter()
+                    .map(|inner_vec| inner_vec.len())
+                    .sum::<usize>() as f32)
+                    / (tokens.len() as f32), // We will add this later
             ));
         }
     }
