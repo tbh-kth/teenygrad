111617cb844f83fe839aeb79bdfdac47ed299425
Jakunot <jabkun@gmail.com>
2024-05-08 19:57:35 +0200

    Add: file structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub mod teenygrad {
+    pub mod features {
+        pub mod multi;
     }
+    pub mod nn {
+        pub mod optim;
+    }
+    pub mod shape {
+        pub mod symbolic;
+    }
+    pub mod function;
+    pub mod lazy;
+    pub mod ops;
+    pub mod realize;
+    pub mod tensor;
 }
