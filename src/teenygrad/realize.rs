90d2d09b3c6416957e158c742762e00f347ea7bb
cab - 21st <60197499+21st-centuryman@users.noreply.github.com>
2024-05-11 22:46:07 +0200

    Fix: realize.rs bugs (#52)

--- src/teenygrad/realize.rs
+++ src/teenygrad/realize.rs
@@ -1,6 +1,6 @@
-pub fn run_schedule(schedule: Vec<i32>) -> std::io::Result<()> {
+pub fn run_schedule(_schedule: Vec<i32>) -> std::io::Result<()> {
     return Ok(());
 }
-pub fn create_schedule(outs: Vec<i32>) -> Vec<i32> {
+pub fn create_schedule(_outs: Vec<i32>) -> Vec<i32> {
     return vec![];
 }
